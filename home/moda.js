04217b39b397cd78a08daaa67d68ebad78f2821e
Stofeles412 <fernandostofeles@gmail.com>
2025-03-20 12:10:09 -0300

    .

--- home/moda.js
+++ home/moda.js
@@ -1,10 +1,14 @@
 function openMenu(){
     var abrir = document.getElementById ("menu-ul")
+    var botao = document.getElementById("botao")
     abrir.classList.toggle ("show")
     if (abrir.classList.contains ("show")){
         abrir.classList.add("show")
+        botao.classList.add("esconder")
     }else{
         abrir.classList.remove("show")
+        botao.classList.remove("esconder")
+       
     }
         
     
